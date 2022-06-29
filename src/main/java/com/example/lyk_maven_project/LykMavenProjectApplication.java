3de4fe6b30bf65d95a82cd95af656ad44cfd592d
lyk19921110 <18326895933@163.com>
2022-06-29 15:49:25 +0800

    测试jenkeins里maven项目的demo

---
+++ src/main/java/com/example/lyk_maven_project/LykMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyk_maven_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LykMavenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LykMavenProjectApplication.class, args);
+    }
+
+}
